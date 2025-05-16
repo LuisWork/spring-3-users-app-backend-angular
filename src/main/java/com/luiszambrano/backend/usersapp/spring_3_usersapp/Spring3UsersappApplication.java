3320adf8f1cbad64c3292a54f16bba3df8fa2df9
LuisWork <luiszambrano.work@gmail.com>
2025-05-16 11:48:19 -0400

    Primer Commit

---
+++ src/main/java/com/luiszambrano/backend/usersapp/spring_3_usersapp/Spring3UsersappApplication.java
@@ -0,0 +1,13 @@
+package com.luiszambrano.backend.usersapp.spring_3_usersapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring3UsersappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring3UsersappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luiszambrano/backend/usersapp/spring_3_usersapp/Spring3UsersappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiszambrano.backend.usersapp.spring_3_usersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring3UsersappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
