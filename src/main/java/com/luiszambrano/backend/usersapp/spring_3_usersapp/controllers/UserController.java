bd836f8507d35e0bf0a01d5ccd7daae2a325040d
LuisWork <luiszambrano.work@gmail.com>
2025-05-16 13:16:10 -0400

    Implementando el Controller REST

---
+++ src/main/java/com/luiszambrano/backend/usersapp/spring_3_usersapp/controllers/UserController.java
@@ -0,0 +1,39 @@
+package com.luiszambrano.backend.usersapp.spring_3_usersapp.controllers;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luiszambrano.backend.usersapp.spring_3_usersapp.models.entities.User;
+import com.luiszambrano.backend.usersapp.spring_3_usersapp.services.UserService;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public List<User> list() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> showById(@PathVariable Long id) {
+        Optional<User> userOptional = service.findById(id);
+        if (userOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(userOptional.orElseThrow());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Collections.singletonMap("Error", "User ID not found"));
+    }
+
+}
