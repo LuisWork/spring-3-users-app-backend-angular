2ddd83e87d8ed44b4d1c4da574b9ff8de28fea66
LuisWork <luiszambrano.work@gmail.com>
2025-05-20 10:31:40 -0400

    Implementando Validaciones en UserEntity

--- src/main/java/com/luiszambrano/backend/usersapp/spring_3_usersapp/models/entities/User.java
+++ src/main/java/com/luiszambrano/backend/usersapp/spring_3_usersapp/models/entities/User.java
@@ -5,6 +5,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "users")
@@ -14,14 +17,21 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String name;
 
+    @NotBlank
     private String lastname;
 
+    @NotBlank
+    @Email
     private String email;
 
+    @NotBlank
+    @Size(min = 5, max = 12)
     private String username;
 
+    @NotBlank
     private String password;
 
     public Long getId() {
