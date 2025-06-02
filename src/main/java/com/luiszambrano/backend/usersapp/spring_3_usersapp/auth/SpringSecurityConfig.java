22e6eb4e3dcca658b0d01c0b037f26ec71791925
LuisWork <luiszambrano.work@gmail.com>
2025-06-02 17:15:00 -0400

    Implementando chain de Seguridad a las rutas permitidas

---
+++ src/main/java/com/luiszambrano/backend/usersapp/spring_3_usersapp/auth/SpringSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.luiszambrano.backend.usersapp.spring_3_usersapp.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http.authorizeHttpRequests(authz -> authz
+                .requestMatchers(HttpMethod.GET, "/api/users", "/api/users/page/{page}")
+                .permitAll()
+                .anyRequest()
+                .authenticated())
+                .csrf(config -> config.disable())
+                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+
+    }
+
+}
--- src/main/java/com/luiszambrano/backend/usersapp/spring_3_usersapp/repositories/UserRepository.java
+++ src/main/java/com/luiszambrano/backend/usersapp/spring_3_usersapp/repositories/UserRepository.java
@@ -5,9 +5,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import com.luiszambrano.backend.usersapp.spring_3_usersapp.models.entities.User;
+import java.util.Optional;
+
 
 public interface UserRepository extends CrudRepository<User, Long>{
 
     Page<User> findAll(Pageable pageable);
+
+    Optional<User> findByUsername(String username);
+    
 
 }
