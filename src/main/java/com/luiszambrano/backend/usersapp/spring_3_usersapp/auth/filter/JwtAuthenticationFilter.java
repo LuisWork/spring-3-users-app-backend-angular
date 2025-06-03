2e75025cc0a0c3af4fddc12be0e185fd27a4ca9f
LuisWork <luiszambrano.work@gmail.com>
2025-06-02 21:06:58 -0400

    Implementando successfulAuthentication y generando el Token jjwt

--- src/main/java/com/luiszambrano/backend/usersapp/spring_3_usersapp/auth/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/luiszambrano/backend/usersapp/spring_3_usersapp/auth/filter/JwtAuthenticationFilter.java
@@ -1,6 +1,9 @@
 package com.luiszambrano.backend.usersapp.spring_3_usersapp.auth.filter;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -13,8 +16,13 @@ import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.luiszambrano.backend.usersapp.spring_3_usersapp.models.entities.User;
 
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import static com.luiszambrano.backend.usersapp.spring_3_usersapp.auth.TokenJwtConfig.SECRET_KEY;
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -43,6 +51,37 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,
                 password);
         return this.authenticationManager.authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+            Authentication authResult) throws IOException, ServletException {
+        org.springframework.security.core.userdetails.User user = (org.springframework.security.core.userdetails.User) authResult
+                .getPrincipal();
+        String username = user.getUsername();
+        String jwt = Jwts.builder()
+                .subject(username)
+                .signWith(SECRET_KEY)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 3600000))
+                .compact();
+
+        response.addHeader("Authorization", "Bearer " + jwt);
+
+        Map<String, String> body = new HashMap<>();
+        body.put("token", jwt);
+        body.put("username", username);
+        body.put("message", String.format("Hello %s, initialized session successfully", username));
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setContentType("application/json");
+        response.setStatus(200);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException failed) throws IOException, ServletException {
+        // TODO Auto-generated method stub
+        super.unsuccessfulAuthentication(request, response, failed);
     }
 
 }
